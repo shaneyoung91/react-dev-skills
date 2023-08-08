8132850a0c2134245edf6d3e71dc2ea60b972680
shaneyoung91 <shaneadam.young@gmail.com>
2023-08-08 12:04:51 -0700

    Lab Part 1 Complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import NewSkillForm from "./NewSkillForm";
 import './App.css'
 
 function App() {
-  
   return (
       <>
-        <div className="App">
           <h1>React Dev Skills</h1>
           <SkillList />
           <hr></hr>
           <NewSkillForm />
-        </div>
       </>
   )
 }
